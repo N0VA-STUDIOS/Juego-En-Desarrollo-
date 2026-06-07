99510440c80511b39416395e050b8271de8b6241
N○VA STUDI●S <elgatovolador3500@gmail.com>
2026-06-07 14:58:10 -0700

    Update enemy.js

--- js/enemy.js
+++ js/enemy.js
@@ -1,18 +1,42 @@
-export default class Enemy {
-    constructor(x, y) {
-        this.x = x; this.y = y; this.hp = 50;
+class Enemy {
+    constructor(x, y, speedMod = 0) {
+        this.x = x; this.y = y;
+        this.width = 30; this.height = 42;
+        this.vy = 0; this.gravity = 0.6;
+        this.speed = 1.2 + speedMod;
+        this.maxHp = 50; this.hp = 50;
+        this.damageCooldown = 0;
     }
 
-    update(player) {
-        // IA simple: seguir al jugador
-        if (this.x < player.x) this.x += 2;
-        else this.x -= 2;
-        if (this.y < player.y) this.y += 2;
-        else this.y -= 2;
+    update(player, groundY) {
+        // Caída por gravedad
+        this.vy += this.gravity;
+        this.y += this.vy;
+
+        if (this.y + this.height >= groundY) {
+            this.y = groundY - this.height;
+            this.vy = 0;
+        }
+
+        if (this.damageCooldown > 0) this.damageCooldown--;
+
+        // Seguimiento inteligente en X hacia la posición del jugador
+        if (this.x < player.x) this.x += this.speed;
+        else this.x -= this.speed;
     }
 
     draw(ctx) {
-        ctx.fillStyle = 'green';
-        ctx.fillRect(this.x, this.y, 30, 30);
+        // Parpadea en rojo si ha recibido daño reciente
+        ctx.fillStyle = this.damageCooldown > 0 ? '#ef4444' : '#22c55e';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+
+        // Renderizado de su barra de salud local
+        const barW = 30;
+        const barH = 5;
+        const currentBarW = (this.hp / this.maxHp) * barW;
+        ctx.fillStyle = '#000';
+        ctx.fillRect(this.x, this.y - 10, barW, barH);
+        ctx.fillStyle = '#ef4444';
+        ctx.fillRect(this.x, this.y - 10, currentBarW, barH);
     }
 }
