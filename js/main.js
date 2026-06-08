2488c9f334d6c951a75f54983e80cc7e6e7ca88a
N○VA STUDI●S <elgatovolador3500@gmail.com>
2026-06-08 05:22:48 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
 // Resoluciones internas fijas para mantener el aspecto del lienzo
-canvas.width = 800;
-canvas.height = 400;
+canvas.width = 1920;
+canvas.height = 1080;
 
 const game = new Game(canvas, ctx);
 
